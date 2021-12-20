35124ae8195f1b98487a667c349b50ed4407d1c2
Rozelin <rozelin.dc@gmail.com>
2021-12-20 09:05:42 +0900

    insert 一部実装

--- avl.cpp
+++ avl.cpp
@@ -9,7 +9,7 @@ Avl::Avl() {
 node_t *Avl::create_new_node(int value) {
   node_t *new_node = (node_t *)malloc(sizeof(node_t));
   if (new_node == NULL) {
-    cerr<<"malloc error!"<<endl;
+    cerr << "malloc error!" << endl;
     exit(1);
   }
 
@@ -24,22 +24,22 @@ node_t *Avl::create_new_node(int value) {
 
 void Avl::print_partial_tree(node_t *partial_root) {
   if (partial_root == NULL) {
-    cout<<".";
+    cout << ".";
     return;
   }
 
-  cout<<"(";
+  cout << "(";
   print_partial_tree(partial_root->left);
-  cout<<" "<<partial_root->value<<" ";
+  cout << " " << partial_root->value << " ";
   print_partial_tree(partial_root->right);
-  cout<<")";
+  cout << ")";
 }
 
-void Avl::print_tree() {
-  print_partial_tree(root);
-}
+void Avl::print_tree() { print_partial_tree(root); }
 
-/** search_value に一致する value を持つノードのポインタを返す。見つからなければ NULL を返す */
+/* search_value に一致する value を持つノードのポインタを返す。
+ * 見つからなければNULL を返す
+ */
 node_t *Avl::search(int search_value) {
   node_t *target = root;
 
@@ -52,4 +52,32 @@ node_t *Avl::search(int search_value) {
   }
 
   return target;
+}
+
+void Avl::insert_node(int insert_value) {
+  node_t *new_node = create_new_node(insert_value);
+
+  if (root = NULL) {
+    root = new_node;
+    return;
+  }
+
+  node_t *target = root;
+
+  while (!(target->left == NULL && target->value >= insert_value) ||
+         !(target->right == NULL && target->value < insert_value)) {
+    if (target->value < insert_value) {
+      target = target->right;
+    } else {
+      target = target->left;
+    }
+  }
+
+  if (target->value < insert_value) {
+    target->right = new_node;
+    new_node->parent = target;
+  } else {
+    target->left = new_node;
+    new_node->parent = target;
+  }
 }
